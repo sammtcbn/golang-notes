a97995ca40c026bb848ab583cb6a10bc1d67e8c9
sam <sammtcbn@gmail.com>
2022-08-30 01:09:03 +0800

    abs example

---
+++ standard-lib-src/abs_ex.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var value float64 = -12.45
+    fmt.Printf ("value is %f\n", value)
+    fmt.Printf ("abs value is %f\n", math.Abs(value))
+}
