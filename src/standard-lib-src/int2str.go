927127c1924cf2b7c93ea64cce6ff972d4e1141b
sammtcbn <sammtcbn@gmail.com>
2024-01-24 22:01:51 +0800

    update integer to string example

--- src/standard-lib-src/int2str.go
+++ src/standard-lib-src/int2str.go
@@ -3,11 +3,19 @@ package main
 import (
     "fmt"
     "strconv"
+    "reflect"
 )
 
 func main() {
     var val int = 2024
-    year := strconv.Itoa(val)
-    fmt.Println(year)
+    fmt.Printf("va; type : %s\n", reflect.TypeOf(val))
+
+    year1 := strconv.Itoa(val)
+    fmt.Println(year1)
+    fmt.Printf("year1 type : %s\n", reflect.TypeOf(year1))
+
+    year2 := fmt.Sprintf("%04d", val)
+    fmt.Println(year2)
+    fmt.Printf("year2 type : %s\n", reflect.TypeOf(year2))
 }
 
